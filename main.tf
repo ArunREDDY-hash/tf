569745ad8b451eac6ab7dc144b05ea66417d35bd
ArunREDDY-hash <75643540+ArunREDDY-hash@users.noreply.github.com>
2025-02-04 18:41:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a897ba00eaed7398" # Replace with your desired AMI ID
+  instance_type = "t2.micro"              # Replace with your desired instance type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
