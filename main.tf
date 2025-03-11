a3f09c96eb10de74c3892dad0946831d641864d0
ArunREDDY-hash <75643540+ArunREDDY-hash@users.noreply.github.com>
2025-03-11 20:45:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired region
+  region = "eu-north-1"  # Replace with your desired region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0a897ba00eaed7398" # Replace with your desired AMI ID
-  instance_type = "t2.micro"              # Replace with your desired instance type
+  ami           = "ami-09a9858973b288bdd" # Replace with your desired AMI ID
+  instance_type = "t3.micro"              # Replace with your desired instance type
 
   tags = {
     Name = "ExampleInstance-arun"
